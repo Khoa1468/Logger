5f0faea6e6afe28f9f11c162efa08f6ea879c659
Khoa1468 <khoalam1468@gmail.com>
2021-10-07 10:53:53 +0700

    update to new version

--- src/LoggerConsole.ts
+++ src/LoggerConsole.ts
@@ -3,24 +3,22 @@ import { IOReturnType, IOErrorParam, IOStd } from "./LoggerInterfaces.js";
 import chalk from "chalk";
 
 export class LoggerConsole extends LoggerMethod {
-  public log(...message: IOStd[]): IOReturnType | undefined {
+  public log(...message: IOStd[]): IOReturnType {
     return this.handleLog("log", message, "Log");
   }
-  public warn(...message: IOStd[]): IOReturnType | undefined {
+  public warn(...message: IOStd[]): IOReturnType {
     return this.handleLog("warn", message, "Warn", (chalk.Color = "yellow"));
   }
-  public error(...message: IOStd[]): IOReturnType | undefined {
+  public error(...message: IOStd[]): IOReturnType {
     return this.handleLog("error", message, "Error", (chalk.Color = "red"));
   }
-  public info(...message: IOStd[]): IOReturnType | undefined {
+  public info(...message: IOStd[]): IOReturnType {
     return this.handleLog("info", message, "Info", (chalk.Color = "cyan"));
   }
-  public fatal<T extends object>(
-    error: IOErrorParam<T>
-  ): IOReturnType | undefined {
+  public fatal<T extends object>(error: IOErrorParam<T>): IOReturnType {
     return this.handleLogFatal<T>(error);
   }
-  public debug(...message: IOStd[]): IOReturnType | undefined {
+  public debug(...message: IOStd[]): IOReturnType {
     return this.handleLog("debug", message, "Debug", (chalk.Color = "green"));
   }
 }
