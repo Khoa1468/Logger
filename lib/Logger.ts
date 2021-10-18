89434b14a9ef3ea051a31f2e8eb34195b2f694f9
Khoa1468 <khoalam1468@gmail.com>
2021-10-18 11:52:44 +0700

    update to new version

--- lib/Logger.ts
+++ lib/Logger.ts
@@ -1,7 +1,31 @@
+import { hostname } from "os";
+import { IOLoggerInterface } from "./LoggerInterfaces.js";
 import { LoggerStatic } from "./LoggerStatic.js";
 
 /**
  * This Is My Logger
  */
 
-export default class Logger extends LoggerStatic {}
+export default class Logger extends LoggerStatic {
+  public static create({
+    instanceName = hostname(),
+    isLoggedAt = true,
+    isType = true,
+    isDisplayRootFile = true,
+    cagetoryName = instanceName,
+    format = "hidden",
+    short = false,
+    levelLog = [0],
+  }: IOLoggerInterface) {
+    return new Logger({
+      instanceName,
+      isLoggedAt,
+      isType,
+      isDisplayRootFile,
+      cagetoryName,
+      format,
+      short,
+      levelLog,
+    });
+  }
+}
